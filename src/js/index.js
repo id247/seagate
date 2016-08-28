98c859268dab1a5f40e922059d9519228073c86c
Aleksand Bazhenov <simplecss@yandex.ru>
2016-08-28 13:35:26 +0300

    slider-added

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,41 @@
 	*/
 
 	function slider(){
+		var $hrefs = $('.' + cssPrefix + 'js-guardians-slider-href');
+		var $slider = $('#' + cssPrefix + 'guardians-slider-list');
 
+		var activeHferClass = cssPrefix + 'guardians__href--active';
+
+		var slider = $slider && $slider.bxSlider({
+			mode: 'vertical',
+			pager: false,
+			controls: false,
+			auto: true,
+			autoHover: true,
+			onSliderLoad: function(){
+				$hrefs
+				.eq(0)
+				.addClass(activeHferClass);
+			},
+			onSlideBefore: function($slideElement, oldIndex, newIndex){
+				$hrefs
+				.removeClass(activeHferClass)
+				.eq(newIndex)
+				.addClass(activeHferClass);
+			}
+		});
+
+		$hrefs.on('click', function(e){
+			e.preventDefault();
+
+			var slideId = $(this).data('slide');
+
+			if (!slideId){
+				slideId = 0;
+			}
+
+			slider && slider.goToSlide(slideId);
+		});	
 	}
 
 
